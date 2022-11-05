90543890efad92b6a6b7d3207519d83fdf19de5f
jacolec <jacwydro@gmail.com>
2022-11-05 20:24:42 +0100

    script update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://tasks-jw-kodilla.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'https://tasks-jw-kodilla.herokuapp.com/v1/tasks';
+  const trelloApiRoot = 'https://tasks-jw-kodilla.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
